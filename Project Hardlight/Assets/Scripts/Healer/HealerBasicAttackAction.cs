7d06b1b0bde30fa2a1a39b3dc6b838da3bc5ac82
Amber Smith <amber.smith@utexas.edu>
2019-09-09 18:42:52 -0500

    Finished healer basic attack setup- heals person most damaged (or random if none damaged)

--- Project Hardlight/Assets/Scripts/Healer/HealerBasicAttackAction.cs
+++ Project Hardlight/Assets/Scripts/Healer/HealerBasicAttackAction.cs
@@ -10,10 +10,10 @@ public class HealerBasicAttackAction : BasicAttackAction
     {
         GameObject healBasic = Instantiate(healBasicAction);
         Fighter thisFighter = GetComponent<Fighter>();
-        healBasic.transform.parent = thisFighter.attackTarget.transform;
+        healBasic.transform.parent = thisFighter.currentTarget.transform;
         healBasic.transform.localPosition = Vector3.zero;
         healBasic.transform.localScale = Vector3.one;
         float healAmt = thisFighter.basicAttackStats.damage + thisFighter.basicAttackStats.damage * thisFighter.attackBoost;
-        thisFighter.attackTarget.GetComponent<Fighter>().Heal(healAmt);
+        thisFighter.currentTarget.GetComponent<Fighter>().Heal(healAmt);
     }
 }
