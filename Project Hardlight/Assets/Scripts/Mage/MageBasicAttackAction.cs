3e04eaae2dd7380305b09125466fd7f3b200c8e5
mindychi <mindychi@gmail.com>
2019-09-10 17:02:39 -0500

    Add animation timing to basic attacks on Mage

--- Project Hardlight/Assets/Scripts/Mage/MageBasicAttackAction.cs
+++ Project Hardlight/Assets/Scripts/Mage/MageBasicAttackAction.cs
@@ -6,7 +6,9 @@ public class MageBasicAttackAction : BasicAttackAction
 {
 
     public GameObject mageBasicAttackPrefab;
-
+    public GameObject spawnPoint;
+    // Used for offsetting to match animation
+    public float animationDelay;
 
     public void Start()
     {
@@ -14,9 +16,19 @@ public class MageBasicAttackAction : BasicAttackAction
 
     public override void DoBasicAttack()
     {
+        StartCoroutine("BasicAttackWithAnimationDelay");
+    }
+    
+    IEnumerator BasicAttackWithAnimationDelay()
+    {
+        // TODO(mchi) scale animation delay to attack speed changes
+        yield return new WaitForSeconds(animationDelay);
+        
         GameObject mageBasicAttack = Instantiate(mageBasicAttackPrefab);
-        mageBasicAttack.transform.position = transform.position;
+        mageBasicAttack.transform.position = spawnPoint.transform.position;
         mageBasicAttack.GetComponent<ProjectileMovement>().source = gameObject;
         mageBasicAttack.GetComponent<ProjectileMovement>().target = GetComponent<Fighter>().currentTarget.gameObject;
+
+        yield break;
     }
 }
