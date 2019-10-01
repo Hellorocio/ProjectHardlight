ba86759a16d77f93da2b49b16bca2fd66199d48e
Huylar Lee <huylarlee@utexas.edu>
2019-10-01 15:54:45 -0500

    Created set new target ability, not hooked up yet

---
+++ Project Hardlight/Assets/Scripts/NewTargetAbility.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewTargetAbility: Ability
+{
+    public float baseEffectRange;
+
+    public GameObject rangeIndicatorPrefab;
+    public GameObject rangeIndicator;
+
+    public GameObject attackTargetUnit;
+
+    private bool targeting;
+
+    private void Update()
+    {
+        if (targeting)
+        {
+            // Update positions
+            rangeIndicator.transform.position = gameObject.transform.position;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+
+    public override bool StartTargeting()
+    {
+        targeting = true;
+
+        rangeIndicator = Instantiate(rangeIndicatorPrefab);
+        rangeIndicator.name = "Range";
+        rangeIndicator.transform.localScale *= 2 * GetRange();
+
+        return true;
+    }
+
+    public override void StopTargeting()
+    {
+        Destroy(rangeIndicator);
+        targeting = false;
+    }
+
+    public override bool DoAbility()
+    {
+        if (selectedTarget != null)
+        {
+            Debug.Log("Set New Target Ability called");
+            Fighter selectedFighter = selectedTarget.GetComponent<Fighter>();
+            if (selectedFighter != null)
+            {
+                // Set Issued Current Target handles the healer check
+                gameObject.GetComponent<FighterAttack>().SetIssuedCurrentTarget(selectedFighter);
+
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public float GetRange()
+    {
+        return baseEffectRange;
+    }
+}
