675593c58aabcc98dfc8de9de6756f523d61ea4a
Huylar Lee <huylarlee@utexas.edu>
2019-10-16 12:19:27 -0500

    Disconnected monsterAI scripts wip

--- Project Hardlight/Assets/Scripts/FireDemonMonster.cs
+++ Project Hardlight/Assets/Scripts/FireDemonMonster.cs
@@ -2,189 +2,171 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// This class implements a fighter's basic attacks
-/// </summary>
-public class FighterAttack : MonoBehaviour
+public class FireDemonMonster : MonoBehaviour
 {
-    public List<CombatInfo.TargetPreference> targetPrefs;
+    public string characterName = "FireDemon";
+    public CombatInfo.Team team = CombatInfo.Team.Enemy;
+    public Animator animator;
+    public float maxHealth;
+    public float maxMana;
+    public float maxAgroRange;
+    public float alertedRange;
+    public float moveSpeed;
+    public float basicAttackDamage;
+    public float attackSpeed;
 
-    // Basic attacking
-    public MonoBehaviour basicAttackAction;
-    public BasicAttackStats basicAttackStats;
-    private bool doBasicAttack;
-    private IEnumerator basicAttackLoop;
+    private float currentHealth;
+    private float currentMana;
 
-    [HideInInspector]
-    public GameObject currentTarget;
-    private BasicAttackAction attack;
-
+    private GameObject currentTarget;
+    //private Grid monsterAIGrid;
     private GameObject attackParent;
 
-    private Fighter fighter;
-    private FighterMove fighterMove;
 
-    // This makes it so there's not weird jittering on the edge of your attack range
-    private static float attackRangeAllowance = 0.1f;
+    public enum MoveState {stopped, moving, interrupted, basicAttacking, advancedAttacking}
+    MoveState moveState = MoveState.stopped;
 
-    //switch target event
-    public delegate void SwitchTarget();
-    public event SwitchTarget OnSwitchTarget;
+
+    public List<CombatInfo.TargetPreference> targetPrefs;
+
+
+    public BasicAttackStats basicAttackStats;
 
     // Start is called before the first frame update
     void Start()
     {
-        fighter = GetComponent<Fighter>();
-        fighterMove = GetComponent<FighterMove>();
-        attack = (BasicAttackAction)basicAttackAction;
-
-        if (fighter.team == CombatInfo.Team.Hero)
-        {
-            attackParent = GameObject.Find("Enemies");
-        }
-        else
-        {
-            attackParent = GameObject.Find("Vessels");
-        }
+        currentHealth = maxHealth;
+        currentMana = 0;
+        //monsterAIGrid = GameObject.Find("MonsterAIGrid").GetComponent<Grid>();
+        attackParent = GameObject.Find("Vessels");
     }
 
-    /// <summary>
-    /// Subscribe to BattleStart event
-    /// </summary>
-    private void OnEnable()
+    // Update is called once per frame
+    void Update()
     {
-        BattleManager battleManager = BattleManager.Instance;
         
-        if (battleManager != null)
-        {
-            battleManager.OnLevelStart += LevelStart;
-        }
     }
 
-    /// <summary>
-    /// Unsubscribe from all events to avoid memory leaks
-    /// </summary>
-    private void OnDisable()
+    void FixedUpdate()
     {
-        if (fighter != null && fighter.battleManager != null)
-        {
-            fighter.battleManager.OnLevelStart -= LevelStart;
-        }
+        MoveToTarget();
+        DecideAttack();
     }
 
-    /// <summary>
-    /// Called when the fighter gets the signal that the battle has started
-    /// Sets targets for heroes, but not enemies
-    /// (UPDATE) Now called directly by BattleManager, not by event because the events were causing problems
-    /// </summary>
-    public void LevelStart ()
+    bool IsValidTarget()
     {
-        //make sure everything has been initialized
-        if (fighter == null)
+        return (currentTarget != null && currentTarget.activeSelf);
+    }
+
+    void DecideAttack()
+    {
+        if (IsValidTarget())
         {
-            Start();
-        }
-        
-        //only start attacking right away if a hero or a fighter with no enemyTrigger
-        if (fighter.team == CombatInfo.Team.Hero || (fighter.team == CombatInfo.Team.Enemy && GetComponentInParent<EnemyTrigger>() == null))
+            if(moveState == MoveState.stopped)
+            {
+                StartBasicAttacking();
+            }
+        } else
         {
-            
             SetCurrentTarget();
         }
     }
 
-    /// <summary>
-    /// Called by FighterMove when this fighter is in range of its current attack target
-    /// </summary>
+    void MoveToTarget()
+    {
+
+        //Vector3 tmp = monsterAIGrid.WorldToCell(currentTarget.transform.position);
+
+        if (IsValidTarget() && !InBasicRangeOfTarget(currentTarget.transform.position))
+        {
+                
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, moveSpeed * Time.deltaTime);
+            moveState = MoveState.moving;
+        } else
+        {
+            moveState = MoveState.stopped;
+        }
+        
+    }
+
     public void StartBasicAttacking()
     {
-        StartCoroutine(BasicAttackLoop());
+        StartCoroutine(BasicAttack());
     }
 
     public void StopBasicAttacking()
     {
-        StopCoroutine(BasicAttackLoop());
+        StopCoroutine(BasicAttack());
     }
 
-    IEnumerator BasicAttackLoop()
+    public void DoBasicAttack(GameObject target)
     {
-        while (currentTarget != null && currentTarget.activeSelf)
-        {
-            //check we are still in range
-            if (!InRangeOfTarget(currentTarget.transform))
-            {
-                fighterMove.StartMoving(currentTarget.transform);
-                break;
-            }
+       
+        target.GetComponent<Fighter>().TakeDamage(basicAttackDamage);
 
-            //attack
-            attack.DoBasicAttack(currentTarget);
-            //&& (fighter.anim.GetCurrentAnimatorStateInfo(0).shortNameHash != Animator.StringToHash("Ability1")) ||
-            //fighter.anim.GetCurrentAnimatorStateInfo(0).shortNameHash != Animator.StringToHash("Ability2")
-            if (fighter.anim != null )
-            {
-                
-                fighter.anim.Play("Attack");
-            }
-
-            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
-            if (audioSource != null && basicAttackStats.sfx != null)
-            {
-                audioSource.clip = basicAttackStats.sfx;
-                audioSource.Play();
-            }
-            yield return new WaitForSeconds(fighter.GetAttackSpeed(basicAttackStats.attackSpeed));
-        }
-
-        //make sure while stopped because currentFighter is gone
-        if (currentTarget == null || !currentTarget.activeSelf)
-        {
-            SetCurrentTarget();
-        }
     }
+
+    IEnumerator BasicAttack()
+    {
+        moveState = MoveState.basicAttacking;
+        DoBasicAttack(currentTarget);
+
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        if (audioSource != null && basicAttackStats.sfx != null)
+        {
+            audioSource.clip = basicAttackStats.sfx;
+            audioSource.Play();
+        }
+        animator.Play("BasicAttack");
+
+        Debug.Log("recorder stop time is : " + animator.recorderStopTime);
+        yield return new WaitForSeconds(animator.recorderStopTime);
+        moveState = MoveState.stopped;
+    }
+
+    //IEnumerator BasicAttackLoop()
+    //{
+    //    while (currentTarget != null && currentTarget.activeSelf)
+    //    {
+    //        //check we are still in range
+    //        if (!InBasicRangeOfTarget(currentTarget.transform.position))
+    //        {
+    //            StopBasicAttacking();
+    //            break;
+    //        }
+
+    //        //attack
+    //        DoBasicAttack(currentTarget);
+    //        //&& (fighter.anim.GetCurrentAnimatorStateInfo(0).shortNameHash != Animator.StringToHash("Ability1")) ||
+    //        //fighter.anim.GetCurrentAnimatorStateInfo(0).shortNameHash != Animator.StringToHash("Ability2")
+
+    //        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+    //        if (audioSource != null && basicAttackStats.sfx != null)
+    //        {
+    //            audioSource.clip = basicAttackStats.sfx;
+    //            audioSource.Play();
+    //        }
+    //        yield return new WaitForSeconds(attackSpeed);
+    //    }
+
+    //    //make sure while stopped because currentFighter is gone
+    //    if (currentTarget == null || !currentTarget.activeSelf)
+    //    {
+    //        SetCurrentTarget();
+    //    }
+    //}
 
     /// <summary>
     /// Returns true if this fighter is in range of their currentTarget
     /// </summary>
     /// <returns></returns>
-    public bool InRangeOfTarget (Transform t, bool useRange = true)
+    public bool InBasicRangeOfTarget(Vector3 t)
     {
-        bool inRange = Vector2.Distance(transform.position, t.position) < basicAttackStats.range + attackRangeAllowance;
-        if (!useRange)
-        {
-            inRange = Vector2.Distance(transform.position, t.position) < attackRangeAllowance;
-        }
+        bool inRange = Vector2.Distance(transform.position, t) < basicAttackStats.range;
+        
         return inRange;
     }
 
-
-    /// <summary>
-    /// Sets the attack target, called when player manually changes attack target
-    /// </summary>
-    public void SetIssuedCurrentTarget(Fighter target)
-    {
-        if (target != null && ((target.team == CombatInfo.Team.Enemy && !HasHealingBasicAttack())
-                || (target.team == CombatInfo.Team.Hero && HasHealingBasicAttack())))
-        {
-            //Updates the current target
-            currentTarget = target.gameObject;
-
-            //invoke OnSwitchTarget event
-            OnSwitchTarget?.Invoke();
-
-            //start moving toward target
-            if (currentTarget != null)
-            {
-                fighterMove.StartMoving(currentTarget.transform);
-            }
-        }
-    }
-
-    /// <summary>
-    /// Searches enemies or players gameObject for a target to attack and sets currentTarget
-    /// Sets currentTarget to null if there are no more things to attack
-    /// Will use targetprefs list if provided, otherwise will default to closest algorithm
-    /// </summary>
     public void SetCurrentTarget()
     {
         //This code chuck below checks if any enemies are active in the scene before calling a targeting function
@@ -270,21 +252,11 @@ public class FighterAttack : MonoBehaviour
             }
         }
 
-        //invoke OnSwitchTarget event
-        OnSwitchTarget?.Invoke();
 
         //start moving toward target
-
-        //Debug.Log("Game obj is " + gameObject.name + " | current target is null? = " + (currentTarget == null));
-        if (currentTarget != null)
-        {
-            fighterMove.StartMoving(currentTarget.transform);
-        }
+        
     }
 
-    /// <summary>
-    /// Finds the weakest enemy and sets current target
-    /// </summary>
     void SetWeakestAttackTarget()
     {
         Fighter[] currentTargets = attackParent.GetComponentsInChildren<Fighter>();
@@ -415,8 +387,10 @@ public class FighterAttack : MonoBehaviour
             return false;
         }
         currentTarget = tempcurrentTarget;
+
         return true;
     }
+
 
     /// <summary>
     /// Finds the closest enemy and sets current target
@@ -463,10 +437,5 @@ public class FighterAttack : MonoBehaviour
             }
         }
         currentTarget = tempcurrentTarget;
-    }
-
-    public bool HasHealingBasicAttack ()
-    {
-        return targetPrefs.Count > 0 && targetPrefs[0] == CombatInfo.TargetPreference.WeakestTeamate;
     }
 }
---
+++ Project Hardlight/Assets/Scripts/MonsterInterface.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface MonsterInterface
+{
+    
+    void setActionNames(string[] names);
+    string[] getActionNames();
+    void callAction1();
+    void callAction2();
+    void callAction3();
+    void callAction4();
+
+
+}
+
+public abstract class MonsterAI : MonoBehaviour, MonsterInterface
+{
+    public bool[] states;
+
+    public abstract void callAction1();
+
+    public abstract void callAction2();
+
+    public abstract void callAction3();
+
+
+    public abstract void callAction4();
+
+    public abstract string[] getActionNames();
+
+    public abstract void setActionNames(string[] names);
+}
