42e6039499d9e840bb751984c6a4c05b5abe21c8
Huylar Lee <huylarlee@utexas.edu>
2019-10-23 15:23:32 -0500

    Fixed mage aoe ui and updated pebblewoods level

--- Project Hardlight/Assets/Scripts/Mage/MageAoEBlast.cs
+++ Project Hardlight/Assets/Scripts/Mage/MageAoEBlast.cs
@@ -34,11 +34,11 @@ public class MageAoEBlast : Ability
 
         rangeIndicator = Instantiate(rangeIndicatorPrefab);
         rangeIndicator.name = "Range";
-        rangeIndicator.transform.localScale *= 2 * GetRange();
+        rangeIndicator.transform.localScale *=  GetRange();
 
         radiusIndicator = Instantiate(radiusIndicatorPrefab);
         radiusIndicator.name = "Radius";
-        radiusIndicator.transform.localScale *= 2 * GetRadius();
+        radiusIndicator.transform.localScale *= GetRadius();
 
         return true;
     }
