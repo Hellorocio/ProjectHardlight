6a704ae51977f55a461f024bf382d86efa0e16e6
Huylar Lee <huylarlee@utexas.edu>
2019-11-04 09:33:01 -0600

    Fixing Tank Monster animations

--- Project Hardlight/Assets/Scripts/TankMonsterAI.cs
+++ Project Hardlight/Assets/Scripts/TankMonsterAI.cs
@@ -57,8 +57,13 @@ public class TankMonsterAI : MonsterAI
             attackZone.SetActive(true);
             //attackZone.transform.LookAt(currentTarget.transform);
             attackZone.transform.position = Vector3.MoveTowards(transform.position, currentTarget.transform.position, 8f);
+            yield return new WaitForSeconds(3.4f);
+            float tmp = basicAttackClipSpeedMultiplier;
+            basicAttackClipSpeedMultiplier = .75f;
+            animator.SetFloat("basicAttackSpeedMultiplier", basicAttackClipSpeedMultiplier);
+            realBasicAttackHitTime = basicAttackHitTime / basicAttackClipSpeedMultiplier;
             yield return new WaitForSeconds(realBasicAttackHitTime);
-
+            
             Collider2D[] hitColliders = new Collider2D[1000];
             Physics2D.OverlapCollider(attackZone.GetComponent<BoxCollider2D>(), myFilter, hitColliders);
             List<GameObject> hitFighters = new List<GameObject>();
@@ -72,7 +77,11 @@ public class TankMonsterAI : MonsterAI
             attackZone.transform.position = transform.position;
             attackZone.transform.rotation = defaultAttackZoneTransform.rotation;
             attackZone.SetActive(false);
+            basicAttackClipSpeedMultiplier = tmp;
+            animator.SetFloat("basicAttackSpeedMultiplier", basicAttackClipSpeedMultiplier);
+            realBasicAttackHitTime = basicAttackHitTime / basicAttackClipSpeedMultiplier;
             yield return new WaitForSeconds(basicAttackClip.length / basicAttackClipSpeedMultiplier - realBasicAttackHitTime);
+            
         }
         
         yield return new WaitForSeconds(timeBetweenAttacks);
