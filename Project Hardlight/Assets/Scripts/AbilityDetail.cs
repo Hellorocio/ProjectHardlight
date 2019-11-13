440ffb913e05f2e830589a1a6aa8c7f3a0d5eefb
Mindy Chi <mindychi@gmail.com>
2019-11-13 03:04:00 -0600

    Loadout shows what the soul does to the abilities

--- Project Hardlight/Assets/Scripts/AbilityDetail.cs
+++ Project Hardlight/Assets/Scripts/AbilityDetail.cs
@@ -9,12 +9,17 @@ public class AbilityDetail : MonoBehaviour
     public TextMeshProUGUI abilityDesc;
     // TODO change to more dynamic things
     public TextMeshProUGUI damage;
+    
     public TextMeshProUGUI sunlightName;
-    public TextMeshProUGUI moonlightName;
-    public TextMeshProUGUI starlightName;
-
+    public TextMeshProUGUI sunlightDesc;
     public Image sunlightIcon;
+    
+    public TextMeshProUGUI moonlightName;
+    public TextMeshProUGUI moonlightDesc;
     public Image moonlightIcon;
+    
+    public TextMeshProUGUI starlightName;
+    public TextMeshProUGUI starlightDesc;
     public Image starlightIcon;
     
     private Color fullOpacity = Color.white;
@@ -28,20 +33,26 @@ public class AbilityDetail : MonoBehaviour
         sunlightName.text = ability.sunlightAugment.augmentTitle;
         moonlightName.text = ability.moonlightAugment.augmentTitle;
         starlightName.text = ability.starlightAugment.augmentTitle;
+        sunlightDesc.text = ability.sunlightAugment.augmentDescription;
+        moonlightDesc.text = ability.moonlightAugment.augmentDescription;
+        starlightDesc.text = ability.starlightAugment.augmentDescription;
     }
 
     public void UpdateSoul(Soul soul)
     {
         Color sunColor = soul.GetAllightValue(AllightType.SUNLIGHT) > 0 ? fullOpacity : halfOpacity;
         sunlightName.color = sunColor;
+        sunlightDesc.color = sunColor;
         sunlightIcon.color = sunColor;
         
         Color moonColor = soul.GetAllightValue(AllightType.MOONLIGHT) > 0 ? fullOpacity : halfOpacity;
         moonlightName.color = moonColor;
+        moonlightDesc.color = moonColor;
         moonlightIcon.color = moonColor;
         
         Color starColor = soul.GetAllightValue(AllightType.STARLIGHT) > 0 ? fullOpacity : halfOpacity;
         starlightName.color = starColor;
+        starlightDesc.color = starColor;
         starlightIcon.color = starColor;
     }
 }
