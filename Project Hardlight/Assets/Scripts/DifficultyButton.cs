cfd2ebfee7d335d97dcb80f3c5e1a49479de9a17
mindychi <mindychi@gmail.com>
2019-12-10 17:28:51 -0600

    Difficulty visibility and made info icon nicer on Loadout UI

---
+++ Project Hardlight/Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    public TextMeshProUGUI myText;
+    public TextMeshProUGUI otherText;
+
+    public void Clicked()
+    {
+        myText.fontStyle = FontStyles.Underline;
+        otherText.fontStyle = FontStyles.Normal;
+    }
+}
